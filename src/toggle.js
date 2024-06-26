85377882adafc27243c9ab342fb1d547854abee6
2405-011-pw <walypa56@gmail.com>
2024-06-26 11:33:10 +0100

    toggle function made

--- src/toggle.js
+++ src/toggle.js
@@ -1,10 +1,19 @@
 /** Adds two given numbers, returning the sum.
- * @param {string} id
+ * @param {string} soughtId
  * @param {object[]} arrayOfObjects
- * @returns {object[]} the sum of a and b
+ * @returns {object[]} new transformed version of arrayOfObjects
  */
-function toggle(id, arrayOfObjects) {
-    return arrayOfObjects
+function toggle(soughtId, arrayOfObjects) {
+    const newArrayOfObjects = []
+    for (let object of arrayOfObjects) {
+        const newObject = {...object}
+        if (soughtId === newObject.id) {
+            newObject.isOn = !newObject.isOn
+        }
+        newArrayOfObjects.push(newObject)
+    }
+
+    return newArrayOfObjects
 }
 
 export { toggle };
